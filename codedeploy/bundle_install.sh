1634c565d315b3ab9adff94ec323cc3f6a503a17
John Drago <john@Johns-MacBook-Pro.local>
2015-02-03 14:03:24 -0800

    Update our bundle_install.sh to properly set the env var BUNDLE_GEMFILE

--- codedeploy/bundle_install.sh
+++ codedeploy/bundle_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cd /var/www/simple-app
-echo "PWD.........................."
-echo `set`
-echo "YAY.........................."
-#BUNDLE_GEMFILE=/var/www/simple-app/Gemfile /usr/local/bin/bundle --deployment
-#BUNDLE_GEMFILE=/var/www/simple-app/Gemfile /usr/local/bin/bundle exec puma -t 0:16 -e staging -d
+
+# Override the value set by AWS CodeDeploy before we try to bundle:
+BUNDLE_GEMFILE=/var/www/simple-app/Gemfile
+
+# Only start puma if bundle installation succeeds:
+bundle --deployment && bundle exec puma -t 0:16 -e staging -d
