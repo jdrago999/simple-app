f8b7f44c11db52064504da21d069fce9ba40c6c0
John Drago <john@Johns-MacBook-Pro.local>
2015-02-03 11:39:19 -0800

    Bundle is being run from the wrong place.  /opt/codedeploy-agent/Gemfile doesn't exists...well...yeah...

--- codedeploy/bundle_install.sh
+++ codedeploy/bundle_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 cd /var/www/simple-app
+echo "PWD.........................."
+echo pwd
+echo "YAY.........................."
 bundle --deployment
 bundle exec puma -t 0:16 -e staging -d
