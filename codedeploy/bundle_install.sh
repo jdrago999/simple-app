f205f8a98d60ec3f36f322208db6b8e054f972ea
John Drago <john@Johns-MacBook-Pro.local>
2015-02-03 11:40:51 -0800

    forgot how echo works

--- codedeploy/bundle_install.sh
+++ codedeploy/bundle_install.sh
@@ -2,7 +2,7 @@
 
 cd /var/www/simple-app
 echo "PWD.........................."
-echo pwd
+echo `pwd`
 echo "YAY.........................."
 bundle --deployment
 bundle exec puma -t 0:16 -e staging -d
