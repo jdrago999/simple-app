f13b9b6bb78068242f8a9d177e9d925d4c64586c
John Drago <john@Johns-MacBook-Pro.local>
2015-02-03 14:14:31 -0800

    Still isn't installing the libssl-dev library.

--- codedeploy/application_start.sh
+++ codedeploy/application_start.sh
@@ -3,6 +3,7 @@
 # Fetch and install the cookbooks we depend on:
 cd /var/www/simple-app/chef
 librarian-chef install
+librarian-chef update
 
 # Apply the chef recipes to this machine:
 cd /var/www/simple-app
