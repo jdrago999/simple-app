cbf430547c105405544411a29b9c0efbd9d6c556
John Drago <john@Johns-MacBook-Pro.local>
2015-02-03 11:20:32 -0800

    Simplify what we do in application_start

--- codedeploy/application_start.sh
+++ codedeploy/application_start.sh
@@ -8,11 +8,5 @@ librarian-chef install
 cd /var/www/simple-app
 /usr/bin/env chef-solo -c chef/solo.rb -j chef/node.json
 
-# This is a deploy-only bundle, so make sure we can write to the necessary folders:
-if [ ! -d log ]; then
-  mkdir log
-  mkdir -p tmp/pid
-fi
-
 chown ubuntu:ubuntu -R /var/www/simple-app
 su ubuntu -c 'cd /var/www/simple-app && bundle install --without test && bundle exec puma -t 0:16 -e staging -d'
