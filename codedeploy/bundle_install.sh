ab6be707ed4ec4373163e7c7f8b5678f68b5bec6
John Drago <john@Johns-MacBook-Pro.local>
2015-02-03 12:54:07 -0800

    Trying to override the BUNDLE_GEMFILE env var

--- codedeploy/bundle_install.sh
+++ codedeploy/bundle_install.sh
@@ -4,5 +4,5 @@ cd /var/www/simple-app
 echo "PWD.........................."
 echo `set`
 echo "YAY.........................."
-bundle --deployment
-bundle exec puma -t 0:16 -e staging -d
+BUNDLE_GEMFILE=/var/www/simple-app/Gemfile /usr/local/bin/bundle --deployment
+/usr/local/bin/bundle exec puma -t 0:16 -e staging -d
