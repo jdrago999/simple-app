78c8a9d16ffa680940c11092b1043cf746f5a045
John Drago <john@Johns-MacBook-Pro.local>
2015-02-03 11:44:10 -0800

    pwd reports that we are in /var/www/simple-app -- trying a different bundle now

--- codedeploy/bundle_install.sh
+++ codedeploy/bundle_install.sh
@@ -4,5 +4,5 @@ cd /var/www/simple-app
 echo "PWD.........................."
 echo `pwd`
 echo "YAY.........................."
-bundle --deployment
-bundle exec puma -t 0:16 -e staging -d
+/usr/local/bin/bundle --deployment
+/usr/local/bin/bundle exec puma -t 0:16 -e staging -d
