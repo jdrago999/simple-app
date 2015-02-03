f22d3704d1c6b05ed48794c5a19cad0576f49bba
John Drago <john@Johns-MacBook-Pro.local>
2015-02-03 15:51:24 -0800

    Let's be more informative about puma not running

--- codedeploy/application_stop.sh
+++ codedeploy/application_stop.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # Kill puma:
-if [ `ps -ef | grep puma | grep -v grep` ]; then
-  kill -s 9 `ps -ef | grep puma | grep -v grep | head -n 1 | awk '{print $2}'`
-fi
+kill -s 9 `ps -ef | grep puma | grep -v grep | head -n 1 | awk '{print $2}'` || echo 'puma not running -- continuing onward'
