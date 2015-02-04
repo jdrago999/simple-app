e01fa45362023ed5ad696de01712da0ce0cec178
John Drago <john@Johns-MacBook-Pro.local>
2015-02-04 09:58:01 -0800

    Fix syntax error in application_stop.  Add puma back to Gemfile

--- codedeploy/application_stop.sh
+++ codedeploy/application_stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Kill puma:
-if [ `ps -ef | grep puma | grep -v grep` ]; then
+if [[ `ps -ef | grep puma | grep -v grep` ]]; then
   kill -s 9 `ps -ef | grep puma | grep -v grep | head -n 1 | awk '{print $2}'` || echo 'puma not running -- continuing onward'
 fi
